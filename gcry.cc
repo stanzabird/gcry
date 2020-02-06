5a2645f14f298b4f4236b5f15131fc7af92e5b60
Bert van der Weerd <bert@stanzabird.nl>
2020-02-06 23:01:18 +0100

    updates

--- gcry.cc
+++ gcry.cc
@@ -1,11 +1,16 @@
 #include <config.h>
-
 #include <iostream>
 #include <vector>
 #include <string>
+#include <unistd.h>
 #include <gcrypt.h>
 
 #define NEED_GCRYPT_VERSION "1.8.5"
+
+static void usage() {
+  std::cerr
+    << "Usage: gcry [-e] [-d] filename\n";
+}
 
 int main(int argc, char* argv[])
 {
@@ -21,5 +26,33 @@ int main(int argc, char* argv[])
   std::cout
     << "info: Using libgcrypt version " << gcry_check_version(nullptr) << ".\n";
 
+  // parse commandline
+  int opt;
+  int cmd_encrypt = 0, cmd_decrypt = 0;
+  
+  while ((opt = getopt(argc,argv, "ed")) != -1) {
+    switch (opt) {
+    case 'e': // encrypt
+      cmd_encrypt = 1;
+      break;
+    case 'd': // decrypt
+      cmd_decrypt = 1;
+      break;
+    default:
+      usage();
+      return 1;
+    }
+  }
+
+  if (optind >= argc) {
+    usage();
+    return 1;
+  }
+
+  const char* filename = argv[optind];
+
+  std::cout
+    << "argument = " << filename << "\n";
+  
   return 0;
 }
