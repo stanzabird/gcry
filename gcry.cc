ae15db747857dc4e7accb86e94f6c9a0a3745668
Bert van der Weerd <bert@stanzabird.nl>
2020-02-09 00:12:41 +0100

    bugfixes

--- gcry.cc
+++ gcry.cc
@@ -7,25 +7,28 @@
 
 #define NEED_GCRYPT_VERSION "1.8.5"
 
-static void usage() {
+int usage() {
   std::cerr
-    << "Usage: gcry [-e] [-d] filename\n";
+    << "Usage: gcry [s.|a.|h] args...\n"
+    << "a: (asymetric)\n"
+    << " e encrypt\n"
+    << " d decrypt\n"
+    << " s sign\n"
+    << "\n"
+    << "s: (symetric\n"
+    << " e encrypt\n"
+    << " d decrypt\n"
+    << "\n"
+    << "h: (hashing)\n"
+    << "\n"
+    << "\n"
+    << "\n"
+    << "\n";
+  return 0;
 }
 
-int main(int argc, char* argv[])
+int symetric(int argc, char* argv[])
 {
-  // initialize gcrypt...
-  if (!gcry_check_version(NEED_GCRYPT_VERSION)) {
-    std::cerr
-      << argv[0] << ": libgcrypt shared library version is too old. (need "
-      << NEED_GCRYPT_VERSION << ", have " << gcry_check_version(nullptr) << ")\n";
-    return 1;
-  }
-  gcry_control(GCRYCTL_INITIALIZATION_FINISHED, 0);
-
-  std::cout
-    << "info: Using libgcrypt version " << gcry_check_version(nullptr) << ".\n";
-
   // parse commandline
   int opt;
   int cmd_encrypt = 0, cmd_decrypt = 0;
@@ -51,8 +54,80 @@ int main(int argc, char* argv[])
 
   const char* filename = argv[optind];
 
-  std::cout
-    << "argument = " << filename << "\n";
+  if (cmd_encrypt)
+    {
+      std::cout << "Encrypting file `" << filename << "'...\n";
+    }
+  else if (cmd_decrypt)
+    {
+      std::cout << "Decrypting file `" << filename << "'...\n";
+    }
+  else
+    {
+      usage();
+      return 1;
+    }
   
+  return 0;
+}
+
+int asymetric(int argc, char* argv[])
+{
+  // parse commandline
+  int opt;
+  int cmd_encrypt = 0, cmd_decrypt = 0;
+  
+  while ((opt = getopt(argc,argv, "ed")) != -1) {
+    switch (opt) {
+    case 'e': // encrypt
+      cmd_encrypt = 1;
+      break;
+    case 'd': // decrypt
+      cmd_decrypt = 1;
+      break;
+    default:
+      usage();
+      return 1;
+    }
+  }
+
+  if (optind >= argc) {
+    usage();
+    return 1;
+  }
+
+  const char* filename = argv[optind];
+
+  if (cmd_encrypt)
+    {
+      std::cout << "Encrypting file `" << filename << "'...\n";
+    }
+  else if (cmd_decrypt)
+    {
+      std::cout << "Decrypting file `" << filename << "'...\n";
+    }
+  else
+    {
+      usage();
+      return 1;
+    }
+  
+  return 0;
+}
+
+int main(int argc, char* argv[])
+{
+  // initialize gcrypt...
+  if (!gcry_check_version(NEED_GCRYPT_VERSION)) {
+    std::cerr
+      << argv[0] << ": libgcrypt shared library version is too old. (need "
+      << NEED_GCRYPT_VERSION << ", have " << gcry_check_version(nullptr) << ")\n";
+    return 1;
+  }
+  gcry_control(GCRYCTL_INITIALIZATION_FINISHED, 0);
+
+  std::cout
+    << "info: Using libgcrypt version " << gcry_check_version(nullptr) << ".\n";
+
   return 0;
 }
